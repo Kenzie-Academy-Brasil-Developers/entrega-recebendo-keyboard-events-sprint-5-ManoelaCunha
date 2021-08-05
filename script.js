163e2281ebe16e3f968f26db1dd707eb07e08de0
Manoela Cunha <manoela_fg@hotmail.com>
2021-08-05 09:14:45 -0300

    atividade finalizada

---
+++ script.js
@@ -0,0 +1,37 @@
+let boxTop = 550;
+let boxLeft = 630;
+
+const img = document.getElementById("box");
+const imgHelicoptero = document.createElement("img");
+imgHelicoptero.src = "../img/helicoptero_right.png";
+img.appendChild(imgHelicoptero);
+
+document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    console.log(keyName);
+    keydownListener(keyName);
+});
+
+const keydownListener = (value) => {
+
+    if(value === 'ArrowDown'){
+        boxTop += 10;
+    }
+
+    if(value === 'ArrowUp'){
+        boxTop -= 10;
+    }
+    
+    if(value === 'ArrowRight'){
+        boxLeft += 10;
+        imgHelicoptero.src = "../img/helicoptero_right.png";
+    }
+
+    if(value === 'ArrowLeft'){
+        boxLeft -= 10;
+        imgHelicoptero.src = "../img/helicoptero_left.png";
+    }
+
+    document.getElementById("box").style.top = boxTop + "px";
+    document.getElementById("box").style.left = boxLeft + "px";
+};
